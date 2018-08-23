89282bef87ba491dfe6c24e20dfb8e06815cebd5
Ralf Graef <graefr@fes.local>
2018-08-23 16:01:29 +0200

    clean up code

--- datenbank.php
+++ datenbank.php
@@ -1,111 +1,108 @@
 <?php
 include_once 'includes/dbconfig.php';
-	if(!$user->is_loggedin())
-	{
-	  $user->redirect('start.php');
-	}
-	$user_id = $_SESSION['user_session'];
-	$stmt = $DB_con->prepare("SELECT * FROM users WHERE user_id=:user_id");
-	$stmt->execute(array(":user_id"=>$user_id));
-	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+if(!$user->is_loggedin()) {
+	$user->redirect('start.php');
+}
+$user_id = $_SESSION['user_session'];
+$stmt = $DB_con->prepare("SELECT * FROM users WHERE user_id=:user_id");
+$stmt->execute(array(":user_id"=>$user_id));
+$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html lang ="en">
 	
-	<?php
-		include_once 'includes/header.php';
-	?>
+<?php
+	include_once 'includes/header.php';
+?>
 
-	<body class="titel2">
+<body class="titel2">
   
-		<?php
-			include_once 'includes/navbar.php';
-		?>	
-		<br>
-			<div class = "container">
-				<br>
-				<br>
+<?php
+	include_once 'includes/navbar.php';
+?>
+
+<br>
+<div class = "container">
+	<br>
+	<br>
+	<h3 class="right">Digitales Zwischenarchiv</h3>
+	<img src="images/fes.png" class = "fes">
+	<hr class = "trenner">
+	<br>
+	<div class ="row"> 
+		<div id = "upload" class="player col-md-12">
+			<table  class="table table-bordered table-striped tabi">
+				<thead>
+					<tr>
+					<th>ID</th>
+					<th>Titel</th>
+					<th>Prüfsumme</th>
+					<th>Speicherort</th>
+					<th>Ingestiert am</th>
+					<th>Bewertung</th>
+					<th>DLZA</th>
+					</tr>
+				</thead>
+				<tbody>
+
+				<?php
 				
-				<h3 class="right">Digitales Zwischenarchiv</h3>
-				<img src="images/fes.png" class = "fes">
-				<hr class = "trenner">
-				<br>
-				<div class ="row"> 
-					<div id = "upload" class="player col-md-12">
-						<table  class="table table-bordered table-striped tabi">
-							<thead>
-								<tr>
-								<th>ID</th>
-								<th>Titel</th>
-								<th>Prüfsumme</th>
-								<th>Speicherort</th>
-								<th>Ingestiert am</th>
-                <th>Bewertung</th>
-								<th>DLZA</th>
-								</tr>
-							</thead>
-							<tbody>
+				$query = "SELECT * FROM daten";       
+				$records_per_page=9;
+				$newquery = $user->paging($query,$records_per_page);
+				$user->dataview($newquery);
+					
+				?>
+				</tbody>
+			</table> 
+			<?php $user->paginglink($query,$records_per_page); ?> 									
+		</div>
+	</div>
+</div>
 
-							<?php
-							
-							$query = "SELECT * FROM daten";       
-							$records_per_page=9;
-							$newquery = $user->paging($query,$records_per_page);
-							$user->dataview($newquery);
-							 
-							?>
-							</tbody>
-						</table> 
-						<?php $user->paginglink($query,$records_per_page); ?> 									
-					</div>
-				</div>
-			</div>
+<script>		
+$('.my_checkbew').on('click', function() {
+	console.log('ID_BEW: ', $(this).attr('id')); 
+	console.log('Checked_BEW: ', $(this)[0].checked );
+	
+	var id= $(this).attr('id');
+	var checked = $(this)[0].checked;
+	
+	var url = 'checkbew.php';
+
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: {id: id, checked: checked},
+		success: function(data){
+				alert(data);
+		}
+	});
+});
+</script>
+
+<script>		
+$('.my_checkdlza').on('click', function() {
+	console.log('ID_DLZA: ', $(this).attr('id')); 
+	console.log('Checked_DLZA: ', $(this)[0].checked );
+	
+	var id= $(this).attr('id');
+	var checked = $(this)[0].checked;
+
+	var url = 'checkdlza.php';
 
 
-			<script>		
-					$('.my_checkbew').on('click', function() {
-							console.log('ID_BEW: ', $(this).attr('id')); 
-							console.log('Checked_BEW: ', $(this)[0].checked );
-						
-					var id= $(this).attr('id');
-          var checked = $(this)[0].checked;
-          
-					var url = 'checkbew.php';
-
-				
-          $.ajax({
-            type: "POST",
-            url: url,
-            data: {id: id, checked: checked},
-            success: function(data){
-                alert(data);
-            }
-          });
-        });
-			</script>
-
-			<script>		
-					$('.my_checkdlza').on('click', function() {
-							console.log('ID_DLZA: ', $(this).attr('id')); 
-							console.log('Checked_DLZA: ', $(this)[0].checked );
-						
-					var id= $(this).attr('id');
-          var checked = $(this)[0].checked;
-          
-					var url = 'checkdlza.php';
-
-				
-          $.ajax({
-            type: "POST",
-            url: url,
-            data: {id: id, checked: checked},
-            success: function(data){
-                alert(data);
-            }
-          });
-        });
-			</script>
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: {id: id, checked: checked},
+		success: function(data){
+				alert(data);
+		}
+	});
+});
+</script>
 
 		
 	</body>
--- ingest.php
+++ ingest.php
@@ -1,104 +1,91 @@
 <!DOCTYPE html>
 <html lang ="en">
 	
-	<?php
-		include_once 'includes/header.php';
-	?>
+<?php
+	include_once 'includes/header.php';
+?>
 
-	<body class = "titel2">
-		<?php
-			include_once 'includes/navbar.php';
-		?>	
-		<br>
-		
-			<div class = "container">
+<body class = "titel2">
+<?php
+include_once 'includes/navbar.php';
+?>	
+<br>
+
+<div class = "container">
+<br>
+<br>
+<h3 class="right">Digitales Zwischenarchiv</h3>
+<img src="images/fes.png" class = "fes">
+<hr class = "trenner">
+<br>
+	<div class="row">
+		<div class="player col-md-6">
+			<h3 class="player">Ingest:</h3> 
 			<br>
+			<?php
+				include_once 'includes/upload.php';
+			?>
+		</div>
+
+		<div class="player col-md-2">
+		</div>
+
+		<div id = "upload" class="tree col-md-4">
+			<h3 class="player">ZwischenArchiv</h3>	
 			<br>
-			<h3 class="right">Digitales Zwischenarchiv</h3>
-			<img src="images/fes.png" class = "fes">
-			<hr class = "trenner">
-			<br>
-				<div class="row">
-					
+			<div id="fileTreeDemo_1" class="demo">
+			</div> 
+		</div>
+	</div>
+</div><!-- /.container -->
 
-					<div class="player col-md-6">
-						<h3 class="player">Dateiupload:</h3> 
-						<br>
-						<?php
-							include_once 'includes/upload.php';
-						?>
-					</div> <!-- /.titel col-md-6 -->
+<script type="text/javascript">
 
-					<div class="player col-md-2">
-					</div>
+$(document).ready( function() {
+	$('#fileTreeDemo_1').fileTree({ root: '../ZwischenArchiv/', 
+		expandedFolders: [	'../ZwischenArchiv/01_Personen/', 
+							'../ZwischenArchiv/02_Organisationen/', 
+							'../ZwischenArchiv/03_Sammlungen/',
+							'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/', 
+							'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/Tonarchiv/',
+							'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/Tonarchiv/Digitalisate/',
+							'../ZwischenArchiv/03_Sammlungen/',
+							'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/', 
+							'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/Plakate/',
+							'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/Plakate/Digitalisate/'
+							], 
+		script: 'connectors/jqueryFileTree.php' }, function(file) { 
+		window.open('./ZwischenArchiv/' + file);
+	});
+});
 
+</script>
+			
 
-					<div id = "upload" class="tree col-md-4">
-						<h3 class="player">Laufwerk B:/</h3>	
-						<br>
-						<div id="fileTreeDemo_1" class="demo">
-						</div> <!-- /.fileTreeDemo_1 -->
-					</div> <!-- /.titel col-md-6-->
+<script type="text/javascript">
+
+$(document).on('change', '.btn-file :file', function() {
+	var input = $(this),
+			numFiles = input.get(0).files ? input.get(0).files.length : 1,
+			label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+	input.trigger('fileselect', [numFiles, label]);
+});
+
+$(document).ready( function() {
+		$('.btn-file :file').on('fileselect', function(event, numFiles, label) {
 				
-
-				</div>
-					
+				var input = $(this).parents('.input-group').find(':text'),
+						log = numFiles > 1 ? numFiles + ' files selected' : label;
 				
-			</div><!-- /.container -->
-
-		<script type="text/javascript">
-			
-
-			$(document).ready( function() {
-				$('#fileTreeDemo_1').fileTree({ root: '../ZwischenArchiv/', 
-					expandedFolders: [	'../ZwischenArchiv/01_Personen/', 
-										'../ZwischenArchiv/02_Organisationen/', 
-										'../ZwischenArchiv/03_Sammlungen/',
-										'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/', 
-										'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/Tonarchiv/',
-										'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/Tonarchiv/Digitalisate/',
-										'../ZwischenArchiv/03_Sammlungen/',
-										'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/', 
-										'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/Plakate/',
-										'../ZwischenArchiv/03_Sammlungen/Audiovisuelle Sammlungen/Plakate/Digitalisate/'
-										], 
-					script: 'connectors/jqueryFileTree.php' }, function(file) { 
-					window.open('./ZwischenArchiv/' + file);
-				});
-			});
-
-			
-		</script>
-			
-		
-		<script>
-			$(document).ready(function(){
-				$('[data-toggle="tooltip"]').tooltip(); 
-			});
-		</script> 
-
-		<script type="text/javascript">
-		$(document).on('change', '.btn-file :file', function() {
-		  var input = $(this),
-		      numFiles = input.get(0).files ? input.get(0).files.length : 1,
-		      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-		  input.trigger('fileselect', [numFiles, label]);
+				if( input.length ) {
+						input.val(log);
+				} else {
+						if( log ) alert(log);
+				}
+				
 		});
+});
 
-		$(document).ready( function() {
-		    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
-		        
-		        var input = $(this).parents('.input-group').find(':text'),
-		            log = numFiles > 1 ? numFiles + ' files selected' : label;
-		        
-		        if( input.length ) {
-		            input.val(log);
-		        } else {
-		            if( log ) alert(log);
-		        }
-		        
-		    });
-		});
-		</script>
-	</body>
+</script>
+</body>
 </html>
